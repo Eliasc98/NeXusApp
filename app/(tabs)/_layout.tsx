33cd2a50b6d854bd717d1de91e4c98bc1e29f148
Godwin <oluwadamilare.c99@gmail.com>
2025-05-28 09:21:52 +0100

    commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -22,44 +22,44 @@ export default function Layout() {
 }
 
 
-// export default function TabLayout() {
-//   const colorScheme = useColorScheme();
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
 
-//   return (
-//     <Tabs
-//       screenOptions={{
-//         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-//         headerShown: false,
-//         tabBarButton: HapticTab,
-//         tabBarBackground: TabBarBackground,
-//         tabBarStyle: Platform.select({
-//           ios: {            
-//             position: 'absolute',
-//           },
-//           default: {},
-//         }),
-//       }}>
-//           <Tabs.Screen
-//         name="LoginScreen"
-//         options={{
-//           title: 'false',
-//           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="index"
-//         // options={{
-//         //   title: 'false',
-//         //   tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-//         // }}
-//       />    
-//       <Tabs.Screen
-//         name="false"
-//         // options={{
-//         //   title: 'Profile',
-//         //   tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
-//         // }}         
-//       />
-//     </Tabs>
-//   );
-// }
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {            
+            position: 'absolute',
+          },
+          default: {},
+        }),
+      }}>
+          <Tabs.Screen
+        name="LoginScreen"
+        options={{
+          title: 'false',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        // options={{
+        //   title: 'false',
+        //   tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        // }}
+      />    
+      <Tabs.Screen
+        name="false"
+        // options={{
+        //   title: 'Profile',
+        //   tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+        // }}         
+      />
+    </Tabs>
+  );
+}
